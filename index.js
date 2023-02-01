ae82346932c23f1b5e16a3c509cb47f26621b5d4
Michael Gioeni <gioenijr@oswego.edu>
2023-02-01 11:18:37 -0700

    5 passing

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction(callBack){
+    callBack();
+    return callBack();
+}
+function returnsANamedFunction (){
+    return returnsANamedFunction;
+}
+function returnsAnAnonymousFunction(){
+    return function () {
+}};
+//function exerciseRoutine(postRunActivity) {
+   // runFiveMiles();
+   //postRunActivity();
+  //}
